3f4503ef45273eb727f2984bd75059cc7a384901
Thomas luechtefeld <tom@insilica.co>
2023-07-20 12:08:10 -0400

    clean up dvc.lock and some code

--- src/1_load_pg_backup.sh
+++ src/1_load_pg_backup.sh
@@ -1,2 +1,18 @@
+# Clear existing Postgres data by deleting the cache/Postgres directory
+rm -rf cache/postgres
+mkdir -p cache/postgres
+
+# Launch a clean Postgres container using Docker Compose up
+# Remove the volume mount from your docker-compose file to make this simpler
+docker-compose up -d
+
+# Provide the Postgres password
 export PGPASSWORD=mypassword
-pg_restore --verbose --clean --no-acl --no-owner -h localhost -p 54321 -U myuser -d mydatabase ./downloads/QSARToolbox46May2023/Database/Toolboxv4v23.backup
+
+# Restore the database from the backup file
+pg_restore --verbose --clean --no-acl --no-owner \
+-h localhost \
+-p 54321 \
+-U myuser \
+-d mydatabase \
+./downloads/QSARToolbox46May2023/Database/Toolboxv4v23.backup
---
+++ src/3_cleanup.sh
@@ -0,0 +1,2 @@
+docker compose down
+rm -rf cache/postgres
